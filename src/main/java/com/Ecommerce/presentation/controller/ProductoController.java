4be1fa4caae8e979bbda9be03591b4b850459b1f
Cliber Castillo <clibercg@gamil.com>
2022-02-06 18:30:52 -0500

    agregando getMapping

--- src/main/java/com/Ecommerce/presentation/controller/ProductoController.java
+++ src/main/java/com/Ecommerce/presentation/controller/ProductoController.java
@@ -3,6 +3,7 @@ package com.Ecommerce.presentation.controller;
 import com.Ecommerce.domain.entity.Producto;
 import com.Ecommerce.domain.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import java.util.Optional;
 public class ProductoController {
     @Autowired
     private ProductoService productoService;
+    @GetMapping
     public List<Producto> GetAll(){
         return productoService.GetAll();
     }
